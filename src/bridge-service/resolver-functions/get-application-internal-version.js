b975b67b904de03e4316ae0d673bddc5bc48c527
Amit Yahav <amit.yahav@testproject.io>
2022-02-01 09:58:06 +0200

    feat: add some logs

--- src/bridge-service/resolver-functions/get-application-internal-version.js
+++ src/bridge-service/resolver-functions/get-application-internal-version.js
@@ -1,5 +1,7 @@
 import { EnvironmentVariables } from "../../shared/environment-variables";
 
 export async function getApplicationInternalVersion() {
+	console.log('EnvironmentVariables.APP_INTERNAL_VERSION', EnvironmentVariables.APP_INTERNAL_VERSION);
+	console.log('process.env.APP_INTERNAL_VERSION', process.env.APP_INTERNAL_VERSION);
 	return EnvironmentVariables.APP_INTERNAL_VERSION;
 }
